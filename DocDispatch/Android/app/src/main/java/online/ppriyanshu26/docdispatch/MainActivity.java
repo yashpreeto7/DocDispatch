c39597f11bf52fa6b505d0f84d7ef8c1b81aa274
Priyanshu <172436044+ppriyanshu26@users.noreply.github.com>
2025-10-28 23:27:33 +0530

    Added CardViews

--- DocDispatch/Android/app/src/main/java/online/ppriyanshu26/docdispatch/MainActivity.java
+++ DocDispatch/Android/app/src/main/java/online/ppriyanshu26/docdispatch/MainActivity.java
@@ -1,12 +1,35 @@
+// MainActivity.java
 package online.ppriyanshu26.docdispatch;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+
+        Button btnSignOut = findViewById(R.id.btnSignOut);
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.signOut();
+                // Go back to OTP screen
+                Intent intent = new Intent(MainActivity.this, OtpActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
